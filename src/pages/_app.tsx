0712011309b5fe6c24728b77d0de6766cea14a2e
AminMarayed <aminmarayed4@gmail.com>
2024-07-12 22:52:36 +0330

    ref: update themeContextProvider and _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,10 @@
+import QueryContextProvider from "@/context/QueryContextProvider";
 import ThemeContextProvider from "@/context/ThemeContextProvider";
 import "@/styles/globals.css";
 import { CssBaseline } from "@mui/material";
-import { QueryClient } from "@tanstack/react-query";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      retry: false,
-      retryOnMount: false,
-      refetchInterval: false,
-    },
-  },
-});
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -30,8 +18,10 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
     <ThemeContextProvider>
-      <CssBaseline />
-      {getLayout(<Component {...pageProps} />)}
+      <QueryContextProvider>
+        <CssBaseline />
+        {getLayout(<Component {...pageProps} />)}
+      </QueryContextProvider>
     </ThemeContextProvider>
   );
 }
