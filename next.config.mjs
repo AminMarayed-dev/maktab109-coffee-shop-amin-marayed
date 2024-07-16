9bbe4ba06a650ff99452431f180954938ab1a163
AminMarayed <aminmarayed4@gmail.com>
2024-07-16 18:29:30 +0330

    ref: update next config and package json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.melocoffee.com",
+        port: "",
+        pathname: "/wp-content/uploads/**/**/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
