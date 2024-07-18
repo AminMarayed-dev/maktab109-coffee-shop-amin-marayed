52e366fe8d1023e07751b123dd47cb5fbf99fb09
AminMarayed <aminmarayed4@gmail.com>
2024-07-18 09:18:28 +0330

    feat: add select and text field items data for table products component

---
+++ src/components/dashboard/table-products/utils/selectItems.data.ts
@@ -0,0 +1,98 @@
+import { localization } from "@/constant/localization";
+
+const {
+  dashboard,
+  common,
+  home: { menuList },
+} = localization;
+
+export const menuSelectItems = {
+  categoryList: [
+    {
+      text: menuList.marayedCoffee,
+      value: "Marayed Coffees",
+    },
+    {
+      text: menuList.foreignCoffee,
+      value: "Foreign Coffees",
+    },
+    {
+      text: menuList.InstantCoffee,
+      value: "Instant coffees",
+    },
+    {
+      text: menuList.accessories,
+      value: "Accessories",
+    },
+    {
+      text: menuList.coffeeMakers,
+      value: "coffee makers",
+    },
+  ],
+  subCategoryList: [
+    {
+      text: menuList.marayedCoffee,
+      value: "Arabica",
+    },
+    {
+      text: menuList.foreignCoffee,
+      value: "Robusta",
+    },
+    {
+      text: menuList.InstantCoffee,
+      value: "Mix",
+    },
+    {
+      text: menuList.accessories,
+      value: "Turk",
+    },
+    {
+      text: menuList.coffeeMakers,
+      value: "Lavaza coffee",
+    },
+    {
+      text: menuList.marayedCoffee,
+      value: "Eli's coffee",
+    },
+    {
+      text: menuList.foreignCoffee,
+      value: "Starbucks coffee",
+    },
+    {
+      text: menuList.InstantCoffee,
+      value: "Mazda coffee",
+    },
+    {
+      text: menuList.accessories,
+      value: "Jacobs coffee",
+    },
+    {
+      text: menuList.coffeeMakers,
+      value: "Davidov coffee",
+    },
+    {
+      text: menuList.marayedCoffee,
+      value: "Nestlé coffee",
+    },
+    {
+      text: menuList.foreignCoffee,
+      value: "Filter coffee",
+    },
+    {
+      text: menuList.InstantCoffee,
+      value: "Grinder coffee",
+    },
+    {
+      text: menuList.accessories,
+      value: "Mokapat",
+    },
+    {
+      text: menuList.accessories,
+      value: "French Press",
+    },
+    {
+      text: menuList.accessories,
+      value: "Electric espresso machine",
+    },
+  ],
+};
---
+++ src/components/dashboard/table-products/utils/textFieldItems.data.ts
@@ -0,0 +1,22 @@
+import { localization } from "@/constant/localization";
+
+const {
+  dashboard,
+  common,
+  home: { menuList },
+} = localization;
+
+export const textFieldItems = [
+  {
+    placeholder: dashboard.nameProduct,
+  },
+  {
+    placeholder: common.price,
+  },
+  {
+    placeholder: common.brand,
+  },
+  {
+    placeholder: common.inventory,
+  },
+];
