c8c8f88f46137beb6bc6b4d0a154755ce00f65b3
AminMarayed <aminmarayed4@gmail.com>
2024-07-18 09:20:57 +0330

    feat: add formatDatePersian , toPersianNumber utils functions

---
+++ src/utils/toPersianNumbers.ts
@@ -0,0 +1,26 @@
+const farsiDigits: string[] = [
+  "۰",
+  "۱",
+  "۲",
+  "۳",
+  "۴",
+  "۵",
+  "۶",
+  "۷",
+  "۸",
+  "۹",
+];
+
+export function toPersianNumbersWithComma(n: number | string) {
+  const numWithCommas = numberWithCommas(n);
+  const persianNumber = toPersianNumbers(numWithCommas);
+  return persianNumber;
+}
+
+function numberWithCommas(x: number | string): string {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+export function toPersianNumbers(n: number): string {
+  return String(n).replace(/\d/g, (x: string) => farsiDigits[parseInt(x, 10)]);
+}
