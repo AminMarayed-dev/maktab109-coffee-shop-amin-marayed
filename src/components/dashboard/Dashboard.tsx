6e3fa0ee1e5c5557c32194f99b7310c088308dea
AminMarayed <aminmarayed4@gmail.com>
2024-07-18 09:18:58 +0330

    ref: update dashboard and handle responsive

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,15 +1,16 @@
+import TableProducts from "@/components/dashboard/table-products/TableProducts";
 import { cssClass } from "@/constant/cssClass";
 import { localization } from "@/constant/localization";
-import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { Container, Tab, Tabs, Typography, useMediaQuery } from "@mui/material";
 import { useState } from "react";
 import { a11yProps, CustomTabPanel } from "./CustomTabPanel";
 import TableInventory from "./TableInventory";
 import TableOrders from "./TableOrders";
-import TableProducts from "./TableProducts";
 
 function Dashboard() {
   const { dashboard } = localization;
   const { center } = cssClass;
+  const matchesMdDown = useMediaQuery((theme) => theme.breakpoints.down("md"));
   const [value, setValue] = useState(0);
   const handleChangeValue = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -28,7 +29,16 @@ function Dashboard() {
   ];
 
   return (
-    <Box sx={{ ...center, flexDirection: "column", gap: 2, mb: 3 }}>
+    <Container
+      sx={{
+        ...center,
+        flexDirection: matchesMdDown ? "column" : "row",
+        gap: matchesMdDown ? 2 : 5,
+        mb: 3,
+        position: "relative",
+        padding: matchesMdDown ? 2 : 0,
+      }}
+    >
       <Typography variant="h3" color="secondary">
         {dashboard.adminPanel}
       </Typography>
@@ -37,6 +47,7 @@ function Dashboard() {
         onChange={handleChangeValue}
         textColor="secondary"
         indicatorColor="primary"
+        orientation={`${matchesMdDown ? "horizontal" : "vertical"}`}
       >
         <Tab label={dashboard.products} {...a11yProps(0)} />
         <Tab label={dashboard.InventoryAndPrices} {...a11yProps(1)} />
@@ -47,7 +58,7 @@ function Dashboard() {
           {tabPanel.component}
         </CustomTabPanel>
       ))}
-    </Box>
+    </Container>
   );
 }
 
