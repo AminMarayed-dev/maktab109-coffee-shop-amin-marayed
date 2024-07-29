1190cf7ec249291d33f6db3390f93e50dec0d47b
AminMarayed <aminmarayed4@gmail.com>
2024-07-29 16:06:44 +0330

    ref: convert single product to ISR render

--- src/pages/shop/[slug]/index.tsx
+++ src/pages/shop/[slug]/index.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { getProductById } from "@/api/dashboard/getProductById.api";
+import { getAllProducts } from "@/api/shared/shared.api";
+import RootLayout from "@/components/layout/root-layout/RootLayout";
+import SingleProduct from "@/components/single-product/SingleProduct";
+import { useQuery } from "@tanstack/react-query";
+import { ReactElement } from "react";
 
-function SingleProductPage() {
-  return <div>shop-SingleProduct</div>;
+function SingleProductPage({ productID, initialData }) {
+  const { data: product } = useQuery({
+    queryKey: ["productID", productID],
+    queryFn: () => getProductById(productID),
+    initialData,
+  });
+  return <SingleProduct product={product} />;
 }
+
+export async function getStaticPaths() {
+  const response = await getAllProducts({ page: 1, limit: "all" });
+  const paths = response?.products.map((product) => ({
+    params: { slug: product._id },
+  }));
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const initialData = await getProductById(params.slug);
+  return { props: { productID: params.slug, initialData }, revalidate: 60 };
+}
+
+SingleProductPage.getLayout = function getLayout(page: ReactElement) {
+  return <RootLayout>{page}</RootLayout>;
+};
 
 export default SingleProductPage;
