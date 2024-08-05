baa54c4cf274291b8ef048e4e56eed08563caa2c
AminMarayed <aminmarayed4@gmail.com>
2024-08-05 10:37:43 +0330

    ref: update zustand for shop

--- src/zustand/shop/store.ts
+++ src/zustand/shop/store.ts
@@ -1,15 +1,20 @@
 import { create } from "zustand";
 
-type State = { selectFilterValue: string };
+type State = { selectFilterValue: string; openDialog: boolean };
 
 type Action = {
   setSelectFilterValue: (selectFilterValue: State["selectFilterValue"]) => void;
+  handleOpenDialog: () => void;
+  handleCloseDialog: () => void;
 };
 
 const useShopStore = create<State & Action>((set) => ({
   selectFilterValue: "",
+  openDialog: false,
   setSelectFilterValue: (selectFilterValue) =>
     set(() => ({ selectFilterValue })),
+  handleOpenDialog: () => set({ openDialog: true }),
+  handleCloseDialog: () => set({ openDialog: false }),
 }));
 
 export default useShopStore;
