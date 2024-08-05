9ee26fc1d0676ba9569b44bd025fbafdf5420568
AminMarayed <aminmarayed4@gmail.com>
2024-08-05 10:33:58 +0330

    feat: create Drawer basket in header component and sub drawer basket

---
+++ src/components/layout/root-layout/header/shared/drawer-basket/ButtonDrawerBasket.tsx
@@ -0,0 +1,47 @@
+import { localization } from "@/constant/localization";
+import { toPersianNumbersWithComma } from "@/utils/toPersianNumbers";
+import { useCartStore } from "@/zustand/cart/store";
+import useHeaderStore from "@/zustand/root-layout/header/store";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+const { cart, common } = localization;
+function ButtonDrawerBasket() {
+  const router = useRouter();
+  const handleCloseDrawer = useHeaderStore((state) => state.handleCloseDrawer);
+  const carts = useCartStore((state) => state.cart);
+  const totalPrice = carts.reduce(
+    (acc, cart) => acc + cart.price * cart.count,
+    0
+  );
+  return (
+    <Stack padding={1} rowGap={2} borderTop={2}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="h6" color="secondary.dark">
+          {cart.total}
+        </Typography>
+        <Typography variant="body1" color="secondary.dark">
+          {toPersianNumbersWithComma(totalPrice)} {common.rial}
+        </Typography>
+      </Box>
+
+      <Button
+        sx={{ color: "primary.main" }}
+        onClick={() => {
+          router.push("/cart");
+          handleCloseDrawer();
+        }}
+      >
+        {cart.showBasket}
+      </Button>
+      <Button onClick={() => router.push("/payment")}>{cart.payment}</Button>
+    </Stack>
+  );
+}
+
+export default ButtonDrawerBasket;
---
+++ src/components/layout/root-layout/header/shared/drawer-basket/CardBasket.tsx
@@ -0,0 +1,60 @@
+import DialogDeleteCart from "@/components/cart/DialogDeleteCart";
+import { localization } from "@/constant/localization";
+import useResponsive from "@/hooks/shared/useResponsive";
+import { toPersianNumbersWithComma } from "@/utils/toPersianNumbers";
+import truncateText from "@/utils/trancateText";
+import { useCartStore } from "@/zustand/cart/store";
+import CloseIcon from "@mui/icons-material/Close";
+import { Divider, IconButton, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+
+const { common } = localization;
+function CardBasket({ card }: { card: any }) {
+  const mdDown = useResponsive({ query: "down", breakpoints: "md" });
+  const handleOpenDialog = useCartStore((state) => state.handleOpenDialog);
+  return (
+    <>
+      <Stack
+        direction="row"
+        columnGap={3}
+        padding={3.2}
+        sx={{
+          cursor: "pointer",
+          position: "relative",
+          "&:hover": { bgcolor: "primary.dark" },
+        }}
+      >
+        <Image
+          width={50}
+          height={50}
+          src={`http://${card.images[0]}`}
+          alt={card.name}
+        />
+
+        <Stack rowGap={1}>
+          <Typography variant="body1">
+            {mdDown ? truncateText(card.name, 20) : truncateText(card.name, 35)}
+          </Typography>
+          <Typography variant="body2">
+            {toPersianNumbersWithComma(card.count)} ×{" "}
+            {toPersianNumbersWithComma(card.price)} {common.rial}
+          </Typography>
+        </Stack>
+        <IconButton
+          sx={{
+            position: "absolute",
+            right: 2,
+            top: 2,
+          }}
+          onClick={handleOpenDialog}
+        >
+          <CloseIcon fontSize="small" sx={{ fill: "#6b7280" }} />
+        </IconButton>
+        <DialogDeleteCart cartID={card._id} />
+      </Stack>
+      <Divider />
+    </>
+  );
+}
+
+export default CardBasket;
