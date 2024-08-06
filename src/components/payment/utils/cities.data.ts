20585f0edf64c46829ce03804596d05b0f9dc157
AminMarayed <aminmarayed4@gmail.com>
2024-08-06 17:04:32 +0330

    feat: add cities data for select payment

---
+++ src/components/payment/utils/cities.data.ts
@@ -0,0 +1,37 @@
+export const cities = [
+  "تهران",
+  "گیلان",
+  "آذربایجان شرقی",
+  "خوزستان",
+  "اصفهان",
+  "کرمانشاه",
+  "خراسان رضوی",
+  "همدان",
+];
+
+export const textFieldItems = [
+  {
+    label: "شهر خود را انتخاب کنید",
+    disabled: false,
+    value: "",
+    placeholder: "نام شهر را به فارسی وارد کنید",
+  },
+  {
+    label: "آدرس کامل خود را وارد کنید",
+    disabled: false,
+    value: "",
+    placeholder: "خیابان و کوچه",
+  },
+  {
+    label: "پلاک و واحد",
+    disabled: false,
+    value: "",
+    placeholder: "شماره واحد ، بلوک ، پلاک (دلخواه)",
+  },
+  {
+    label: "کد پستی",
+    disabled: false,
+    value: "",
+    placeholder: "کد 11 رقمی با اعداد انگلیسی",
+  },
+];
