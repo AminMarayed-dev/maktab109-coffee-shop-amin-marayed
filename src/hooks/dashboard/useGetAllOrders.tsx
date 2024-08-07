9d253267271e8ee2a2c5543be0074869f2ffc708
AminMarayed <aminmarayed4@gmail.com>
2024-08-07 11:22:35 +0330

    ref: update use get all orders hook

--- src/hooks/dashboard/useGetAllOrders.tsx
+++ src/hooks/dashboard/useGetAllOrders.tsx
@@ -1,9 +1,14 @@
 import { getAllOrders } from "@/api/shared/shared.api";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetAllOrdersToDashboard = ({ page, limit }) => {
+export const useGetAllOrdersToDashboard = ({
+  page,
+  limit,
+  time,
+  deliveryStatus,
+}) => {
   return useQuery<any>({
-    queryKey: ["orders-dashboard", page, limit],
-    queryFn: () => getAllOrders({ page, limit }),
+    queryKey: ["orders-dashboard", page, limit, time, deliveryStatus],
+    queryFn: () => getAllOrders({ page, limit, time, deliveryStatus }),
   });
 };
