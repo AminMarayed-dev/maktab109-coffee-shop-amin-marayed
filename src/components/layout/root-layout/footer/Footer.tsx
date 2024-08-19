72584c7d353a8b0b25481a71c753c8d6754f0eb6
AminMarayed <aminmarayed4@gmail.com>
2024-08-19 14:49:53 +0330

    ref: update ui footer

--- src/components/layout/root-layout/footer/Footer.tsx
+++ src/components/layout/root-layout/footer/Footer.tsx
@@ -9,22 +9,24 @@ function Footer() {
       lg={12}
       xs={12}
       sx={{ bgcolor: "primary.dark" }}
-      padding={1}
+      padding={3.5}
       rowGap={1.5}
     >
       {listItemsFooter.map((item, index) => (
         <Grid item lg={3} xs={6} key={index} textAlign="center">
-          <Typography variant="h5" mb={2}>
+          <Typography variant="h6" mb={2}>
             {item.title}
           </Typography>
-          <Stack rowGap={0.5}>
+          <Stack rowGap={1}>
             {item.hasImage ? (
               <Box>
                 <Image src={item.imageURL ?? ""} alt={item.title} />
               </Box>
             ) : (
               item.body?.map((text, i) => (
-                <Typography key={i}>{text}</Typography>
+                <Typography key={i} variant="body1">
+                  {text}
+                </Typography>
               ))
             )}
           </Stack>
