b7cf77c512d904576041272a338d939a2d50a8df
AminMarayed <aminmarayed4@gmail.com>
2024-09-03 14:33:42 +0330

    ref: refactor config.ts

--- src/api/config/config.ts
+++ src/api/config/config.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { deleteCookie, getCookie, setCookie } from "cookies-next";
 
 const app = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_PRODUCTION,
-  baseURL: process.env.NEXT_PUBLIC_API_DEVELOP,
+  baseURL: process.env.NEXT_PUBLIC_API_PRODUCTION,
+  // baseURL: process.env.NEXT_PUBLIC_API_DEVELOP,
 });
 
 export const api = {
